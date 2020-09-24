af13a3dc722d4b9c0ae274584fb6dc6347961a32
Shazad0304 <shazadshiraz97@gmail.com>
2020-09-25 04:37:59 +0500

    heroku

--- src/main/java/com/example/crypto/mongorepo/connection.java
+++ src/main/java/com/example/crypto/mongorepo/connection.java
@@ -1,6 +1,7 @@
 package com.example.crypto.mongorepo;
 
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
@@ -8,10 +9,12 @@ public class connection {
 
     public static MongoOperations mongoOps;
 
+
     public static MongoOperations get(){
         if(mongoOps == null){
-            mongoOps = new MongoTemplate(MongoClients.create(), "fyp");
+            mongoOps = new MongoTemplate(MongoClients.create("mongodb+srv://Fyp:fyp12345@cluster0.3v8b6.mongodb.net/fyb?retryWrites=true&w=majority"), "fyp");
         }
         return mongoOps;
     }
+
 }
